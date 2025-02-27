00563d676ae17893b3dc23eb2c6de565048d890c
Taylor Ivanoff <taylor.ivanoff@vocus.com.au>
2025-02-27 13:56:32 +0100

    Updated for local dev

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://example-store-api.test/api",
+  baseURL: "http://127.0.0.1:8000/api",
 });
 
 API.interceptors.request.use((config) => {
