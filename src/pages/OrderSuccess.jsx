0960996296d0cb3d61c40b653cf110dac3875c84
Taylor Ivanoff <taylor.ivanoff@vocus.com.au>
2025-02-27 18:42:05 +0100

    Changed order success link

--- src/pages/OrderSuccess.jsx
+++ src/pages/OrderSuccess.jsx
@@ -5,7 +5,7 @@ const OrderSuccess = () => {
     <div>
       <h2>Order placed successfully!</h2>
       <p>Thank you for your order!</p>
-      <Link to="/">Go to Home</Link>
+      <Link to="/orders">Go to Orders</Link>
     </div>
   );
 };
